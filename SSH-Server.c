3e2897cb0daa3b8ee6cbda85724b343c79140dd7
root <atrautrim7@gmail.com>
2019-05-24 11:03:56 -0400

    new SSH channel

--- SSH-Server.c
+++ SSH-Server.c
@@ -1,15 +1,24 @@
+
+/*
+ * Author: Andrew Trautrim
+ * Remote SSH server using libssh library
+ */
+
 #include <libssh/libssh.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <windows.h>
 
+void closeSession(char *errorMesg, ssh_sesion session);
+
 int main(int argc, char **argv) {
 	
 	// checks for proper input
-	if(argc != 4) {
-		printf("Usage: %s <user> <passwd> <host>\n", argv[0]);
+	if(argc != 4 && argc != 5) {
+		printf("Usage: %s <user> <passwd> <host> [port]\n", argv[0]);
 		return -1;
 	}
+
 	char *user = argv[1];
 	char *passwd = agv[2];
 	char *host = argv[3];
@@ -17,44 +26,60 @@ int main(int argc, char **argv) {
 	int port = (argv[4] == NULL) ? 22 : atoi(argv[4]);
 
 	// create new session
-	ssh_session currSession = ssh_new();
-	if(currSession == NULL) {
-		printf("Unable to create session\n");
+	ssh_session session = ssh_new();
+	if(session == NULL) {
+		printf("Unable to create session: %s\n", ssh_get_error(session));
 		return -1;
 	}
+
 	// modify session settings
-	ssh_options_set(currSession, SSH_OPTIONS_HOST, host);
-	ssh_options_set(currSession, SSH_OPTIONS_PORT, port);
-	ssh_options_set(currSession, SSH_OPTIONS_USER, user);
+	ssh_options_set(session, SSH_OPTIONS_HOST, host);
+	ssh_options_set(session, SSH_OPTIONS_PORT, port);
+	ssh_options_set(session, SSH_OPTIONS_USER, user);
 
 	// connect to server
-	int rc = ssh_connect(currSession);
+	int rc = ssh_connect(session);
+	// verify connection
 	if (rc != SSH_OK) {
-		printf("Unable to connect to %s: %s\n", host, ssh_get_error(currSession));
-		ssh_free(currSession);
+		closeSession("Unable to connect to host", session);
 		return -1;
 	}
 
 	// host verification
-	if(verify_knownhost(currSession) < 0) {
-		ssh_disconnect(currSession);
-		ssh_free(currSession);
+	if(verify_knownhost(session) < 0) {
+		closeSession("Unknown host", session)
 		return -1;
 	}
 
-	// user authentication
-	rc = ssh_userauth_password(currSession, NULL, passwd);
+	// password authentication
+	rc = ssh_userauth_password(session, user, passwd);
 	if (rc != SSH_AUTH_SUCCESS) {
-		printf("Password authentication error: %s", ssh_get_error(currSession));
-		ssh_disconnect(currSession);
-		ssh_free(currSession);
+		closeSession("Password authentication error", session);
+		return -1;
+	}
+
+	// Create remote shell
+	ssh_channel channel = ssh_channel_new(session);
+	if(channel == NULL) {
+		closeSession("Unable to create channel", session);
 		return -1;
 	}
 
 	// TODO
+	// Open remote shell/ pass command
 
-	ssh_disconnect(currSession);
-	ssh_free(currSession);
-
+	closeSession(NULL, session);
 	return 1;
+}
+
+/*
+ * prints error message if needed
+ * disconnects/deallocates ssh session
+ */
+void closeSession(const char *errorMesg, ssh_session session) {
+	if (errorMesg != NULL) 
+		printf("%s: %s\n", errorMesg, ssh_get_error(session));
+	ssh_disconnect(session);
+	ssh_free(session);
+	return;
 }
